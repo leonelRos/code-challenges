288ad42f9d463eb0f010df4fe14d7bb456e4db33
Leonel Rosales <rleonel426@gmail.com>
2019-12-11 17:13:45 -0800

    second commit

--- js/main.js
+++ js/main.js
@@ -21,4 +21,36 @@ function multiply(a,b) {
    }
    console.log(getCount('this is it'));
    console.log(getCount('what are you talking about'));
+
+
+// const arr =[1,2,3,4];
+
+// for( let i = 0; i < arr.length; i++) {
+//   sum = i += arr.length
+// console.log(sum)
+// }
+
+// function titleCase (title, minorWords){
+//   if(!title) return ' '
+//   minorWords = minorWords ? minorWords.toLowerCase().split(' ') : []
+//     return title.toLowerCase().split(' ').map((e,i) => {
+//       if(i==0|| !minorWords.includes(e)) return e.replace(e[0],e[0].toUpperCase())
+//       return e;
+//     })
+// }
+//   join(' ');
+
+
+var cubes = [
+    [1, 2, 3],
+    [4, 5, 6],    
+    [7, 8, 9],
+   ];
+   
+   for(var i = 0; i < cubes.length; i++) {
+       var cube = cubes[i];
+       for(var j = 0; j < cube.length; j++) {
+           display ("cube[" + i + "][" + j + "] = " + cube[j]);
+       }
+   }
    
