878daf9a8e527242553564f296ffcedb612f560b
Leonel Rosales <rleonel426@gmail.com>
2019-12-16 18:12:10 -0800

    linked list adding two numbers

--- js/main.js
+++ js/main.js
@@ -318,3 +318,14 @@ var greetHi = greetDeeplyCurried("hello")(", ");
 greetHi(".")("eddie");
 greetHi(".")("heidi");
 // Above is just a little syntax to keep on practicing!!!
+
+
+ //practice LINKED LIST
+// You are given two non-empty linked lists representing two non-negative integers. 
+// The digits are stored in reverse order and each of their nodes contain a single digit. 
+// Add the two numbers and return it as a linked list.
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+//EXAMPLE: 
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
+// Explanation: 342 + 465 = 807.
