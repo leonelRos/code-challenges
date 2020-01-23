75f51946d66a750697df71af519da2777a4ecfa3
Leonel Rosales <rleonel426@gmail.com>
2020-01-22 21:58:02 -0800

    challenges

--- js/main.js
+++ js/main.js
@@ -501,3 +501,34 @@ function isPalindrome(str) {
 //   }
 //   return true;
 // }
+
+/*-----------------------------------------------------------------
+Challenge: 12-hammingDistance
+
+Difficulty: Intermediate
+
+Prompt:
+
+In information theory, the hamming distance refers to the count of the differences between two strings of equal length.  It is used in computer science for such things as implementing "fuzzy search"  capability.
+
+- Write a function named hammingDistance that accepts two arguments which are both strings of equal length.
+- The function should return the count of the symbols (characters, numbers, etc.) at the same position within each string that are different.
+- If the strings are not of the same length, the function should return NaN.
+
+Examples:
+
+hammingDistance('abc', 'abc'); //=> 0
+hammingDistance('a1c', 'a2c'); //=> 1
+hammingDistance('!!!!', '****'); //=> 4
+hammingDistance('abc', 'ab'); //=> NaN
+-----------------------------------------------------------------*/
+// Your solution for 12-hammingDistance here:
+
+function hammingDistance(s1, s2) {
+  if (s1.length !== s2.length) return NaN;
+  var count = 0;
+  for (var i = 0; i < s1.length; i++) {
+    if (s1.charAt(i) !== s2.charAt(i)) count++;
+  }
+  return count;
+}
