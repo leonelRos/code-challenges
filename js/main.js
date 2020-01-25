33385c86c0e839caa13fce7cc09e5e0abfa414b2
Leonel Rosales <rleonel426@gmail.com>
2020-01-25 14:17:25 -0800

    challenges13

--- js/main.js
+++ js/main.js
@@ -641,3 +641,53 @@ function mergeObjects(target, ...objects) {
   });
   return target;
 }
+/*-----------------------------------------------------------------
+Challenge: 16-findHighestPriced
+
+Difficulty:  Intermediate  
+
+Prompt:
+
+- Write a function named findHighestPriced that accepts a single array of objects.
+- The objects contained in the array are guaranteed to have a price property holding a numeric value.
+- The function should return the object in the array that has the largest value held in the price property.
+- If there's a tie between two or more objects, return the first of those objects in the array.
+- Return the original object, not a copy.
+
+Examples:
+
+findHighestPriced([
+  { sku: 'a1', price: 25 },
+  { sku: 'b2', price: 5 },
+  { sku: 'c3', price: 50 },
+  { sku: 'd4', price: 10 }
+]);
+//=> { sku: 'c3', price: 50 } 
+
+findHighestPriced([
+  { sku: 'a1', price: 25 },
+  { sku: 'b2', price: 50 },
+  { sku: 'c3', price: 50 },
+  { sku: 'd4', price: 10 }
+]);
+//=> { sku: 'b2', price: 50 }
+-----------------------------------------------------------------*/
+// Your solution for 16-findHighestPriced here:
+
+function findHighestPriced(arr) {
+  var highestPrice = 0;
+  var resultObj;
+  arr.forEach(function(item) {
+    if (item.price > highestPrice) {
+      highestPrice = item.price;
+      resultObj = item;
+    }
+  });
+  return resultObj;
+}
+
+/*--- using the reduce Array method ---*/
+// function findHighestPriced(arr) {
+//   return arr.reduce((highest, item) => item.price > highest.price ? item : highest);
+// }
+
