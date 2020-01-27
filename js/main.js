73633c3386f04b5360ba28113921d063119aa8f9
Leonel Rosales <rleonel426@gmail.com>
2020-01-26 21:16:57 -0800

    challenges13

--- js/main.js
+++ js/main.js
@@ -822,3 +822,30 @@ function flatten(arr) {
 //   return arr.reduce((flatArr, elem) => flatArr.concat(Array.isArray(elem) ? flatten(elem): elem), []);
 // }
 
+/*-----------------------------------------------------------------
+Challenge: 20-isPrime
+
+Difficulty: Intermediate
+
+Prompt:
+
+- Write a function named isPrime that returns true when the integer argument passed to it is a prime number and false when the argument passed to it is not prime.
+- A prime number is a whole number (integer) greater than 1 that is evenly divisible by only itself.
+
+Examples:
+
+isPrime(2) //=> true
+isPrime(3) //=> true 
+isPrime(4) //=> false
+isPrime(29) //=> true
+isPrime(200) //=> false
+-----------------------------------------------------------------*/
+// Your solution for 20-isPrime here:
+
+function isPrime(n) {
+  if (n < 2 || !Number.isInteger(n)) return false;
+  for (var i = 2; i <= n / 2; i++) {
+    if (Number.isInteger(n / i)) return false;
+  }
+  return true;
+}
