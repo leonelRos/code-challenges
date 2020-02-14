45b2d4cb247b7532edbdb320506e6a4c0e3e941f
Leonel Rosales <rleonel426@gmail.com>
2020-02-13 21:17:29 -0800

    challenges

--- js/main.js
+++ js/main.js
@@ -1146,4 +1146,41 @@ function toCamelCase(str) {
 //   return str.replace(/[_-]\w/g, match => match.charAt(1).toUpperCase());
 // }
 
+
+/*-----------------------------------------------------------------
+Challenge: 27-countTheBits
+
+Difficulty:  Intermediate
+
+Prompt:
+
+- Write a function called countTheBits that accepts a single numeric argument that will be an integer.
+- The function should return the number of bits that are set to 1 in the number's binary representation.
+
+Hints:
+
+- We typically work with "decimal" numbers on a daily basis. Decimal is "base 10", where there are 10 digits available - 0 thru 9.  However, it's binary that computers understand - 1's and 0's.  The 1's and 0's are called bits.
+- As an example, the decimal value of 13 is represented in binary as 1101.  There are 3 one bits and 1 zero bit in the decimal number of 13.
+- Carefully read the documentation for the Number.prototype.toString method.
+
+Examples:
+
+countTheBits( 0 ) // => 0
+countTheBits( 13 ) // => 3
+countTheBits( 256 ) // => 1
+countTheBits( 255 ) //=> 8
+countTheBits( 65535 )  //=> 16
+-----------------------------------------------------------------*/
+// Your solution for 27-countTheBits here:
+
+function countTheBits(int) {
+  return int.toString(2).split('').filter(bit => bit === '1').length;
+}
+
+/* Using a regular expression. Note || operator to provide an array so that can use length if there are no matches */
+// function countTheBits(int) {
+//   return (int.toString(2).match(/1/g) || []).length;
+// }
+
+
     
