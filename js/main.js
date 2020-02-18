4eb48e4d0d42cd801ece44f551dc3734c1c613e6
Leonel Rosales <rleonel426@gmail.com>
2020-02-17 22:41:17 -0800

    more challenges

--- js/main.js
+++ js/main.js
@@ -1240,4 +1240,32 @@ function gridTrip(xyArr, moves) {
 //   });
 //   return result;
 // }
+
+
     
+// A complete binary tree is a binary tree that each level except possibiliy the last level, is completed filled. 
+// Suppose you are giving a binary tree represented as an array. For example, [3, 6, 2, 9, -1, 10] retpresents the 
+// following binary tree, where -1 indicates it is a NULL node.
+//Write a function that determines whether the left or right branch of the tree is larger. The size of each branch
+//  is the sum of the node vlaues. The function should return the string “Right” if the right side is larger and “Left” if the left side is larger.
+//  If the tree has zero nodes or if the size of the branches are equal, an empty string “” should be returned.
+
+// solution
+const solution = (arr) => {
+  if (!arr) return "";
+  if (arr.length === 0){
+      return "";
+  }
+  var sum = (arr, i) => {
+      if(i - 1 < arr.length) {
+          if (arr[i - 1] === -1){
+              return 0;
+          }
+          return arr[i - 1] + sum(arr, i * 2) + sum(arr, i * 2 + 1);
+      }
+      return 0;
+  }
+  const left = sum(arr,2);
+  const right = sum(arr, 3);
+  return (left == right) ? "" : (left > right ? "Left" : "Right");
+};
