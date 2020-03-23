74601ebed3d5b7b74a309c507f2d93ac551b4f91
Leonel Rosales <rleonel426@gmail.com>
2020-03-22 21:11:18 -0700

    new code challenge

--- js/main.js
+++ js/main.js
@@ -1429,3 +1429,14 @@ var findMedianSortedArrays = function(nums1, nums2) {
 };
 
 // how to pick a random element from a infinite stream
+import random
+
+def pick(big_stream):
+  random_element = None
+
+    for i, e in enumerate(big_stream):
+        if i == 0:
+            random_element = e
+        if random.randint(1, i + 1) == 1:
+            random_element = e
+    return random_element
