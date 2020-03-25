b048f2aeffb29411875122aee487c1f82bb2851e
Leonel Rosales <rleonel426@gmail.com>
2020-03-24 17:12:26 -0700

    new code challenge

--- js/main.js
+++ js/main.js
@@ -1476,3 +1476,26 @@ function longestPalindrome (s) {
   return maxLen;
 }
 
+// John works at a clothing store. He has a large pile of socks that he must pair by color for sale.
+//  Given an array of integers representing the color of each sock, determine how many
+//  pairs of socks with matching colors there are.
+
+// For example, there are n = 7 socks with colors ar=[1,2,1,2,1,3,2]. 
+// There is one pair of color 1 and one of color 2. 
+// There are three odd socks left, one of each color. The number of pairs is 2.
+
+// find the pair of socks
+function sortedCount (n, ar){
+  //built in sort() method
+let sorting = ar.sort((a, b) => a - b) 
+let result = 0;
+// we take in to the last index
+  for( i =0; i < n - 1; i++){
+    //we compare box indexes and increment the result
+    if (sorting[i] === sorting[i + 1]){
+      result ++;
+      i += 1;
+    }
+  }
+  return result;
+} 
