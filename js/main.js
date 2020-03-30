c44241d6176ba7944b3c72321bf5eea527ffbcba
Leonel Rosales <rleonel426@gmail.com>
2020-03-29 21:19:05 -0700

    substring of a palindrome

--- js/main.js
+++ js/main.js
@@ -1499,3 +1499,96 @@ let result = 0;
   }
   return result;
 } 
+
+// Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+// Example 1:
+// Input: "babad"
+// Output: "bab"
+// Note: "aba" is also a valid answer.
+// Example 2:
+// Input: "cbbd"
+// Output: "bb"
+
+function expandAroundCenterSolution(s) {
+  let start = 0, end = 0;
+  for (let i = 0; i < s.length; i++) {
+      let center = getCenter(s, i);
+      let bounds = expandAroundCenter(s, center[0], center[1]);
+      let L = bounds[0], R = bounds[1];
+      if (R - L > end - start) {
+          start = L;
+          end = R;
+      }
+      console.log("---");
+      i = center[1]; //move to the end of center, i++ will then shift pointer to index right after current center
+  }
+  return s.substring(start, end + 1);
+}
+
+function expandAroundCenter(s, left, right) {
+  let L = left, R = right;
+
+  while (L >= 0 && R < s.length && s[L] === s[R]) {
+      L--;
+      R++;
+  }
+  console.log("expand return " + (L+1) + ":" + (R-1));
+  return [++L, --R];
+}
+
+function getCenter(s,c){
+  let L = c, R = c;
+  console.log("get center start index: " + c);
+  while(s[L] === s[++R] && R <= s.length );
+  console.log("return " + L + ":" + (R-1));
+  return [L, --R];
+}
+
+// BRUTE FORCE
+function isPalindrome(s) {
+  let k = 0;
+  let l = s.length - 1;
+  let isPalindrome = true;
+  while(k<=l) {
+      if (!(s.charAt(k) === s.charAt(l))) {
+          isPalindrome = false;
+          break;
+      }
+      k++;
+      l--;
+  }
+  return isPalindrome;
+}
+
+function bruteForce(s) {
+
+  let maxPalindrome = "";
+  
+  for (let i = 0; i <= s.length-1; i++) {
+      for (let j = i+1; j <= s.length; j++) {
+          let subStr = s.substring(i, j)
+          console.log("check: " + subStr);
+          if (isPalindrome(subStr)) {
+              console.log(subStr + " is palindrome")
+              if (subStr.length > maxPalindrome.length) {
+                      maxPalindrome = subStr;
+              }
+          } 
+      }
+      console.log("---");
+  }  
+  
+  return maxPalindrome;
+}
+
+function longestPalindrome(s) {
+
+  // let maxPalindrome = bruteForce(s);
+  let maxPalindrome = expandAroundCenterSolution(s);
+
+  return maxPalindrome;
+}
+
+let testCase = "cccaba";
+console.log("Longest Palindromic Substring: " + longestPalindrome(testCase));
